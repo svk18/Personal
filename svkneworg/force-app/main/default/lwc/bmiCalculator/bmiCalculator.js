580fb8311f63a3988e1437f7c45d4fc6394c0aba
svk18 <souvik.banik18@gmail.com>
2024-02-06 21:39:05 +0530

    BMI calculator

---
+++ svkneworg/force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -0,0 +1,29 @@
+import { LightningElement,track } from 'lwc';
+
+
+export default class BmiCalculator extends LightningElement {
+
+    cardTitle = 'BMI Calculator';
+    weight;
+    height;
+    bmi;
+
+    onWeightChange(event){
+        this.weight = parseFloat(event.target.value);
+    }
+
+    onHeightChange(event){
+        this.height = parseFloat(event.target.value);
+    }
+
+    calculateBMI(){
+        try{
+            this.bmi = this.weight/(this.height*this.height);
+        }
+        catch(error){
+            this.bmi = undefined;
+        }
+    }
+
+    
+}
